aa64c61a9cd702d8c1c719c1947c95acda766e6c
Артем <artem@melchakov>
2018-09-10 18:33:53 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsDb::Engine => '/rails/db', :as => 'rails_db'
   root to: "home#show"
   resources :subscriptions, only: [:create]
   resources :calls, only: [:create]
