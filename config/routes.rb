b38847b37498e7209528fcf770959ed2d1a35474
Артем <artem@melchakov>
2018-09-11 10:46:33 +0300

    Delete rails db gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount RailsDb::Engine => '/rails/db', :as => 'rails_db'
   root to: "home#show"
   resources :subscriptions, only: [:create]
   resources :calls, only: [:create]
